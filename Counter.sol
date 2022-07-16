434fd42e6ccc165974973f37e7b9106ce2955a7d
whatTHEh3ll <47017391+whatTHEh3ll@users.noreply.github.com>
2022-07-16 09:13:36 -0700

    interface

--- Counter.sol
+++ Counter.sol
@@ -1,15 +1,14 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13.0;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
 contract Counter {
-   uint public count;
+	uint public count;
+	
+	function inc() external {
+		count += 1;
+	}
 
-   function inc() external {
-       count += 1;
-   }
-
-   function dec() external {
-       count -= 1;
-   }
-
+    function dec() external {
+    	count -= 1;
+    }
 }
